dcfd6bdaf0afe46341911e56213d4706cf879a04
木下天音ヤフー <a.fallkan-24@softbank.ne.jp>
2018-02-07 11:46:05 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by 木下天音 on 2018/02/07.
+//  Copyright © 2018年 Amanerune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
